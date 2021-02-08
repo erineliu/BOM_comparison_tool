c6c19a5ff14f0bf847e2a5b01e74c29a5b68ccde
Ernie Liu <ernie_liu@wistron.com>
2021-02-08 10:37:59 +0800

    modified clear_image.sh

--- clear_image.sh
+++ clear_image.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker image list | grep pyinstaller_img:latest && docker rmi pyinstaller_img:latest
+docker image list | grep -E "pyinstaller_img.*latest" && docker rmi pyinstaller_img:latest
